43b70e90b589f7886c0f1af9d1c672ed825877c4
LambdaGaming <starwars18m@gmail.com>
2020-04-10 18:42:19 -0400

    Finished conversion

--- pepsi.py
+++ pepsi.py
@@ -1,80 +1,62 @@
 import discord
-import random
 
-BadWords = [
-	"communism",
-	"china",
-	"ussr",
-	"stalin",
-	"lennin",
-	"putin",
+Drinks = [
+	"coke",
+	"sprite",
+	"coca-cola",
+	"coca cola",
+	"lemonade",
+	"fanta",
+	"beer",
+	"surge",
+	"vault",
+	"water", 
+	"tropicana",
+	"bleach",
 	"vodka",
-	"commie",
-	"russia",
-	"cuba",
-	"vietnam",
-	"mao",
-	"castro",
-	"bernie",
-	"kim",
-	"korea",
-	"california",
-	"red",
-	"cyka",
-	"blyat",
-	"communist",
-	"gulag",
-	"chinese",
-	"vietnamese",
-	"korean",
-	"californian",
-	"reds",
-	"communists",
-	"gulags",
-	"vodkas",
-	"blizzard"
+	"dr pepper",
+	"dr. pepper",
+	"mtn dew",
+	"mountain dew"
 ]
 
-Quotes = [
-	"Weapons: hot.",
-	"Mission: the destruction of any and all Chinese communists.",
-	"America will never fall to communist invasion.",
-	"Obstruction detected. Composition: titanium alloy supplemented by photonic resonance barrier.",
-	"Probability of mission hindrance: zero percent.",
-	"Democracy.... is non-negotiable.",
-	"Death is a preferable alternative to communism.",
-	"Communist detected on American soil. Lethal force engaged.",
-	"Tactical assessment: Red Chinese victory-impossible.",
-	"Communism is the very definition of failure.",
-	"Communism is a temporary setback on the road to freedom.",
-	"Embrace democracy or you will be eradicated.",
-	"Democracy will never be defeated.",
-	"Primary Targets: any and all Red Chinese invaders.",
-	"Emergency Communist Acquisition Directive: immediate self destruct. Better dead, than Red."
+GoodDrinks = [
+	"pep",
+	"pepsi",
+	"bepis"
 ]
 
 class MyClient( discord.Client ):
 	async def on_ready( self ):
-		randfallout = str( random.randint( 3, 4 ) )
-		await client.change_presence( activity = discord.Game( name = "Fallout " + randfallout ) )
 		print( 'Logged in as {0}!'.format( self.user ) )
 
-		for chan in client.get_all_channels():
-			if chan.name == "general-kenobi":
-				await chan.send( "LIBERTY PRIME IS ONLINE." )
-				break
-			if chan.name == "general":
-				await chan.send( "LIBERTY PRIME IS ONLINE." )
-
 	async def on_message( self, message ):
+		usertag = str( message.author.id )
+		split = message.content.split( " " )
+		offenseFile = open( "data/drinkoffenses/" + usertag + ".json", "w+" )
 		if message.author.bot: return
-		if "hong kong" in message.content.lower():
-			await message.channel.send( "LIBERATE HONG KONG, REVOLUTION OF OUR AGE!" )
-			return
-		for item in BadWords:
+		if split[0] == "!pepsiman":
+			try:
+				if split[1] == " ":
+					await message.channel.send( "Yes? What do you want?" )
+				elif split[1] == "givepepsi":
+					await message.channel.send( 'https://media.giphy.com/media/s0s2S5MhxBD4A/giphy.gif' )
+				elif split[1] == "showlatestoffense":
+					await message.channel.send( "Last recorded message containing a drink offense: " + "`" + offenseFile.read() + "`" )
+				return
+			except IndexError:
+				await message.channel.send( "Yes? What do you want?" )
+				return
+		for item in Drinks:
 			if item in message.content.lower():
-				await message.channel.send( Quotes[ random.randint( 0, len( Quotes ) ) ].upper() )
+				await message.channel.send( "YOU FOOL! HOW DARE YOU DRINK " + item.upper() + " INSTEAD OF PEPSI!" )
+				offenseFile.write( message.content )
 				break
+		for item in GoodDrinks:
+			if item in message.content.lower():
+				await message.channel.send( "https://i.pinimg.com/originals/30/da/4e/30da4e74b1d08a8b65c1dcbbae44b546.jpg" )
+		offenseFile.close()
+		cooldown = False
 
 try:
 	token = open( "token.txt" )
