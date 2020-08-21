dc68823b4547c79e2d81827d4d37faa29f73fa58
LambdaGaming <starwars18m@gmail.com>
2020-08-20 21:44:20 -0400

    Added run file

---
+++ pepsi/run.bat
@@ -0,0 +1,6 @@
+:: This is used to run the bot in the background automatically. (Windows only) ::
+:: Place a shortcut to this file in your start menu startup folder to have it run when your PC starts. ::
+
+@echo off
+start "" pyw pepsi.py
+exit
